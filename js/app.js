8dfb7fc1ea0b3d043141dedf97a48b3b7d971ff2
Owen Holman <owenstholman@gmail.com>
2019-06-14 16:19:45 -0700

    Commenting

--- js/app.js
+++ js/app.js
@@ -1,62 +1,72 @@
+// constant declaration for html elements
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+// setting the canvas dimensions to the screen dimensions
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// constant declarations for html elements
 const gameBtn = document.getElementById('game-btn');
 const cover = document.getElementById('cover-content');
 const game = document.getElementById('game-content');
+
+// variable declarations (booleans, objects)
 var animation = null;
 var rain;
 var initGen = false;
 
-window.addEventListener('resize', () => {
+// begin of event listeners
+window.addEventListener('resize', () => { // triggers on browser resize
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-});
+}); // end of 1st event listener
 
-window.addEventListener('keydown', e => {
-	switch(e.code) {
+window.addEventListener('keydown', e => { // triggers when the user presses a key
+	switch(e.code) { // start of switch conditional (grabs the key code of the key that the user pressed)
 		case 'Enter':
-			gameBtn.click();
+			gameBtn.click(); // runs when the user presses enter
 		break;
-	}
-})
+	} // end of switch conditional
+});
+// end of event listeners
 
-gameBtn.addEventListener('click', () => {
-	if (initGen == false) {
+gameBtn.addEventListener('click', () => { // adds event listener to html button that triggers when clicked
+	if (initGen == false) { // start if statement; checks if the rain has been generated yet
+		// displays the proper html elements
 		cover.style.display = 'none';
 		game.style.display = 'block';
-		rain = new RainObj();
-		play();
-		initGen = true;
-	} else if (initGen == true) {
-		window.cancelAnimationFrame(animation);
-		delete rain;
+
+		rain = new RainObj(); // sets predefined rain variable to rain object (in rain.js)
+		play(); // calls play function
+		initGen = true; // sets boolean generated to true
+	} else if (initGen == true) { // if the rain has already been generated
+		window.cancelAnimationFrame(animation); // cancels the animation (pausing)
+		delete rain; // deletes / recycles object
+		// displays the respective html elements
 		cover.style.display = 'flex';
 		game.style.display = 'none';
-		initGen = false;
-	}
-});
+		initGen = false; // sets boolean generated to false
+	} // end of if statement
+}); // end of gameBtn event listener
 
-const drawBackground = () => {
-	let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+const drawBackground = () => { // start drawBackground function
+	let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height); // local variable declared for the background
+	// for the createLinearGradient function. Start/end points of the gradient
 	gradient.addColorStop(0, "#0B1F26");
 	gradient.addColorStop(1, "#263238");
-	return gradient;
+	return gradient; // returns the gradient
 }
 
-const play = () => {
-	// recommence the loop
-	animation = window.requestAnimationFrame(play);
+const play = () => { // start play function
+	animation = window.requestAnimationFrame(play); // recommence the loop
 
 	// redraw the background
 	ctx.fillStyle = drawBackground();
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	// object manipulation
+	// object manipulation updates every frame
 	rain.init();
 	rain.update();
 	rain.draw();
-}
+} // end of play function
--- js/rain.js
+++ js/rain.js
@@ -1,57 +1,60 @@
-const RainObj = function() {
+const RainObj = function() { // begin rainObj function
+	// begin this. declarations
 	this.total = [];
 	this.maxRain = 65;
 	this.bounceTotal = 0;
-
 	this.puddles = [];
 
+	// for each rain particle
 	this.x;
 	this.y;
 	this.dy;
 	this.length = 10;
 	this.friction = 0.2;
 
-	const getRandomNum = (min, max) => {
-		return Math.random() * (max - min) + min;
-	}
+	const getRandomNum = (min, max) => { // start function getRandomNum
+		return Math.random() * (max - min) + min; // returns a random number within the argued range
+	} // end function getRandomNum
 
-	this.init = () => {
-		if (this.total.length < this.maxRain) {
+	this.init = () => { // start this. function init
+		if (this.total.length < this.maxRain) { // begin if statement; checks if the array length is less than the max amount
+			// creates another particle
 			this.x = getRandomNum(0, canvas.width);
 			this.y = 0;
 			this.dy = 1;
-			this.total.push({
+			this.total.push({ // pushes to array
 				x: this.x,
 				y: this.y,
 				dy: this.dy,
 				length: this.length,
 				bounce: this.bounceTotal
-			});
-		}
-	}
+			}); // end of traits being pushed
+		} // end of if statement
+	} // end of this.init function
 
-	this.update = () => {
-		for (i = 0; i < this.total.length; i++) {
-			if (this.total[i].bounce > 2) {
-				this.puddles.push({
+	this.update = () => { // begin this.update function
+		for (i = 0; i < this.total.length; i++) { // begin for loop; iterates through every object in the array
+			if (this.total[i].bounce > 2) { // if the rain has bounced 3 times
+				this.puddles.push({ // pushes the x and y of that object to a new array called puddles
 					x: this.total[i].x,
 					y: this.total[i].y
-				});
-				this.total.splice(i, 1);
-			} else {
-				if (this.total[i].y + this.total[i].length >= canvas.height * getRandomNum(11/12, 13/12)) {
-					this.total[i].dy = -this.total[i].dy * this.friction;
-					this.total[i].bounce++;
-				} else {
-					this.total[i].dy += 0.981;
-				}
-				this.total[i].y += this.total[i].dy;
-			}
-		}
-	}
+				}); // end of array push
+				this.total.splice(i, 1); // splices (removes) the original from the original rain array
+			} else { // runs if the rain has not bounced yet
+				if (this.total[i].y + this.total[i].length >= canvas.height * getRandomNum(11/12, 13/12)) { // start of if statement; checks if the rain has fallen to a specific spot
+					this.total[i].dy = -this.total[i].dy * this.friction; // the rain bounces up by reversing its velocity upwards and slowing it down with a friction constant
+					this.total[i].bounce++; // iterates the bounce variable
+				} else { // runs if the rain has not reached its full bounce height
+					this.total[i].dy += 0.981; // rain accelerates at an acceleration 10 times smaller than real gravity
+				} // end of if statement
+				this.total[i].y += this.total[i].dy; // increases the y value by the acceleration
+			} // end of else statement
+		} // end of for loop
+	} // end of this.update function
 
-	this.draw = () => {
-		for (i = 0; i < this.total.length; i++) {
+	this.draw = () => { // start this.draw function
+		for (i = 0; i < this.total.length; i++) { // start for loop; draws rain particles
+			// the context canvas paths for each rain particle
 			ctx.strokeStyle = '#79C4F2';
 			ctx.beginPath();
 			ctx.moveTo(this.total[i].x, this.total[i].y);
@@ -59,11 +62,11 @@ const RainObj = function() {
 			ctx.lineCap = "round";
 			ctx.lineTo(this.total[i].x, this.total[i].y + this.total[i].length);
 			ctx.closePath();
-			ctx.stroke();
-		}
-		for (i = 0; i < this.puddles.length; i++) {
+			ctx.stroke(); // finally paints it
+		} // end of for loop
+		for (i = 0; i < this.puddles.length; i++) { // start for loop; draws puddles
 			ctx.fillStyle = '#79C4F2';
 			ctx.fillRect(this.puddles[i].x, this.puddles[i].y, 1, 1);
-		}
-	}
-}
+		} // end of for loop
+	} // end of this.draw function
+} // end of rainObj object function
